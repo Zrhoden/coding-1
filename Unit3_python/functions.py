92388c040141fba7eec783a2f17854998b8b242e
Zrhoden <zrhoden157@boyslatin.net>
2024-11-19 14:36:17 +0000

    functions

---
+++ Unit3_python/functions.py
@@ -0,0 +1,32 @@
+# What is a function?
+
+# A fucntion is a set of intructions that preforms a specific 
+# task or job
+
+# there are two variations of functions in Python
+# Built-in functions- instructions that are pre-written in python programming language
+
+# print() is a built in function. When we pass data it will output it into the terminal automatically
+print('Coding Class') 
+#input() is a built in function. it allows us to write and pass data into our programs from the terminal
+name = input('what is your name')
+print(name)
+
+number = int(input('please provide a numver'))
+print(23*number)
+#  User Defined Functions
+
+#Function Syntax - how it is written
+
+#this is a function definition
+def sandwhichinstructions():
+    print('step 1. get 2 peices of bread.')
+    print('step 2. putting ingredients inside of bread')
+    print('step 3. put the bread with ingredients together')
+
+# there are two states of a user defined function;
+    # the function definition
+    # the function call
+
+# this is a function call
+sandwhichinstructions()
