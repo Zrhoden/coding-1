1d451b9f9afecafe7ddbc4eaf8bc0fb8ca4cf056
Zrhoden <zrhoden157@boyslatin.net>
2024-12-04 14:35:23 +0000

    handsOnFunctions (2)

--- Unit3_python/handsOnFunctions.py
+++ Unit3_python/handsOnFunctions.py
@@ -23,5 +23,26 @@ def verifyAge(age):
     print('Congrats! you can buy GTA VI')
    else:
         print('Sorry, you need an adult to buy this game.')
-
 verifyAge(19)
+
+
+def hoursToMinutes(hour):
+   print('minutes: 60')
+   print(hour * 60)
+
+
+hoursToMinutes(10)
+
+# Conditional Statements
+# If /else keywords: gives us the ability to control outcomes and make decisions on data.
+# food expiration software is an example of using conditonal statements. If the food expires it needs to be thrown away, otherwise, or else it can be eaten.
+def foodExpiration(month, date, year):
+    expirationMonth = 12
+    expirationDate = 5
+    expirationYear = 2026
+    if date > expirationDate and year > expirationYear:
+       print('throw food away')
+    else:
+       print('food is still good')
+
+foodExpiration(12, 8, 2024)
