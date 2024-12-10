bc05a18745e9bbfb6cf5204caaa27d9f4bd6d092
Zrhoden <zrhoden157@boyslatin.net>
2024-12-10 13:51:21 +0000

    Async Activity Dec 6

---
+++ Unit3_python/asyncActivityDec6.py
@@ -0,0 +1,18 @@
+def reverseWord(word):
+    print(reverseWord('pet'))
+    print('tep')
+    print(reverseWord('book'))
+    print('koob')
+
+
+def stateLandmark(state):
+    if state == "pennsylvania":
+        print("A landmark in Pennsylvania is the Liberty Bell, a symbol of American Independence.")    
+    else: state
+    print("new york")
+    print("A landmark in New York is the Statute if Liberty, a representation of freedom and democracy") 
+    if state == "california":
+         print("A landmark in California is the Hollywood Walk of Fame, celebrating the entertainment industry and pop culture.") 
+    else: state
+    print("texas")
+    print("A landmark in Texas is The Alamo, a historic site of the Texas Revolution")
