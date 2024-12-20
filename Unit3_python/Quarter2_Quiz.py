db7198d378d5f42530151621c05fa16bce2b3452
Zrhoden <zrhoden157@boyslatin.net>
2024-12-20 14:36:15 +0000

    Quarter2 Quiz

---
+++ Unit3_python/Quarter2_Quiz.py
@@ -0,0 +1,16 @@
+# 1
+print('A function is a block of code that runs only when called.')
+# 2
+print ('A parameter is the variable listend inside the parntheses while an argument is the vaule sent to the function when its called')
+# 3
+print('An if/else conditonal statement is something used to filter and create outcomes based on data')
+# 4
+print('An integer data is  is a whole number, positive or negative, without decimals, of unlimited length.')
+# 5
+print('A boolean data type is used to tell you if something is true or false')
+#6
+print('A comparison operator')
+#7
+print('An assignment operator')
+#8
+
