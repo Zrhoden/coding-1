7043bf1df38974a40de48d3887e9604214fe8cd9
Zrhoden <zrhoden157@boyslatin.net>
2024-12-20 16:36:02 +0000

    Quarter 2 Quiz

--- Unit3_python/Quarter2_Quiz.py
+++ Unit3_python/Quarter2_Quiz.py
@@ -13,4 +13,24 @@ print('A comparison operator')
 #7
 print('An assignment operator')
 #8
-
+def checkPasswordLength( length, password):
+    if length (password) > 10:
+        return('your password is too long')
+    elif length (password) < 4:
+        return('your password is too short')
+    else:
+        return('your password was created successfully')
+#9
+def federalIncomeTax(earnings):
+    if earnings == '0 - 12,000':
+        return ('you will be taxed 10% plus an additional 3% for the state')
+    elif earnings == '12,001 - 50,000':
+        return(' you will be taxed 12% plus an additonal 3% for the state')
+    elif earnings == '50,001 - 103,000':
+        return('you will be taxed 22% plus an addtional 3% for the state')
+    elif earnings == '103,001 - 198,000':
+        return ('you will be taxed 23% plus an addtional 3% for the state')
+    else :
+        return('error')
+#10 
+    
