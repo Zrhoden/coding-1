684436ed724402ea76ed9a902622625f789263f3
Zrhoden <zrhoden157@boyslatin.net>
2024-12-20 00:20:13 +0000

    Holiday Extra Credit

---
+++ Unit3_python/holidayExtraCredit.py
@@ -0,0 +1,40 @@
+# Holiday Extra Credit
+# 12/19/24
+
+def holidayPlanner(date):
+     if date == '12/21/2024':
+        return "on saturday dec. 21, I'm going to chill."
+     elif date == '12/22/2024':
+        return "on sunday dec. 22, I will go back to my mom's house."
+     elif date == '12/23/2024':
+         return "on monday dec. 23, I will be going to the poconos with my family."
+     elif date == '12/24/2024':
+         return "on tuesday dec. 24, I will most likely go snowtubing and go out to dinner."
+     elif date == '12/25/2024':
+         return "on wednesday dec. 25, I will help my mom make dinner."
+     elif date == '12/26/2024':
+         return "on thursday dec. 26, nothing is planned for this day."
+     elif date == '12/27/2024':
+         return "on friday dec. 27, I will be coming back to Philly."
+     elif date == '12/28/2024':
+         return "on saturday dec. 28, my mom will be hosting her 2nd annual family pajama party."
+     elif date == '12/29/2024':
+         return "on sunday dec. 29, I will be going back to my dad's house."
+     elif date == '12/30/2024':
+         return "on monday dec. 30, nothing is planned for this day."
+     elif date == '12/31/2024':
+         return "on tuesday dec. 31, I'm going to a New Year's party."
+     elif date == '1/1/2025':
+          return "on wednesday jan. 1, nothing is planned for this day."
+     elif date == '1/2/2025':
+         return "on thursday jan. 2, nothing is planned for this day."
+     elif date == '1/3/2025':
+         return "on friday jan. 3, nothing is planned for this day."
+     elif date == '1/4/2025':
+        return "on saturday jan. 4, nothing is planned for this day."
+     elif date == '1/5/2025':
+         return "on sunday jan. 5, I will be attending the Eagles vs. Giants game."
+     elif date == '1/6/2025':
+         return "on monday jan. 6, nothing is planned for this day."
+     else:
+          return "nothing is planned for this day."
