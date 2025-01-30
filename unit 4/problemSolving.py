109f17b74c9f59b486458b6d9c53178ab2269a62
Zrhoden <zrhoden157@boyslatin.net>
2025-01-30 14:35:49 +0000

    Classwork (Problem Solving)

---
+++ unit 4/problemSolving.py
@@ -0,0 +1,58 @@
+# CALCULATOR APP
+# You have been tasked with developing a calculator 
+# app that students can use during their standardized test. 
+# The calculator should be able to take in a number, followed by an 
+# arithmetic operator and then another number. Once your 
+# program has all of these components, it should return the answer. 
+
+#  1. What is the problem asking you to do?
+'your answers should be in string format...'
+
+# 2. What are some keywords and phrases ? 
+
+# 3. Is there input data that the question provides ?
+
+# 4. What is the desired output ?
+
+# 5. Pseudocode Steps & Additional Notes 
+
+# Solution
+
+# ROCK, PAPER, SCISSOR GAME APP
+# You are working on a game concept with several 
+# other engineers and you need to provide them with a
+#  simple proof of concepts. 
+# For now, you have suggested that your 
+# game work on a RPS (rock, paper, scissors) loop. 
+# When a user plays, rock, paper, or scissors, 
+# the computer will randomly select one of the 3 
+# options as well. Your program will then determine 
+# who won, based on the values. The game should only 
+# allow the player to play 3 hands, and inform the 
+# player if the won or lost (best 2 out of 3 format.) 
+
+#  1. What is the problem asking you to do?
+'we need a basic rps game with a best 2 out of 3 format'
+# 2. What are some keywords and phrases ? 
+'random select from program'
+# 3. Is there input data that the question provides ?
+'we need to take in the users RPS selection'
+# 4. What is the desired output ?
+'shows which player won or loss'
+# 5. Pseudocode Steps & Additional Notes 
+' import random, random.range() random.choice'
+'needs to have logic to determine who won after three turns, as well ask the player if they want to play again WE NEED TO LOOP'
+'if / else conditions to determine which options win, ex i user selects rock and cpu selects paper == cpu wins'
+'Need a variable to track if user or cpu wins'
+
+# Solution
+def rpsGame():
+    print('what would you like to choose?')
+
+
+
+
+
+
+
+rpsGame()
