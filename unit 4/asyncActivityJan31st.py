00317f960e1fe7042ceed681e5000992d851b238
Zrhoden <zrhoden157@boyslatin.net>
2025-02-03 14:04:27 +0000

    Async Activity January 31st

---
+++ unit 4/asyncActivityJan31st.py
@@ -0,0 +1,29 @@
+#1
+
+# In a complete sentence, what is the problem asking you to do ?
+'The problem is asking me to build a grade checker that returns a letter grade based on a number input from 0 to 100' 
+'as well as averaging out the grade as the user continues to add grades'
+# Where there any parts of the question that don't make sense, if so write them down in complete sentences.
+'no'
+# Please write down the keywords and phrases that will help you solve this problem ?
+'letter grade, grade checker'
+# Does the problem provide you with any input data? Does it tell you what type of data your program needs to take in ?
+'Yes, you are supposed to input a number'
+# Does the problem provide you with desired output data? Does it tell you that your program needs to give somthing back to the user ?
+'Yes, you are supposed to get a return of a letter grade'
+# Using Pseudocode (fake code), write down Step by step how you think you can solve this problem. Remember, this does not have to be code.
+'add class, input number grade, get back letter grade, continue'
+#2
+#  1. What is the problem asking you to do?
+'we need a basic rps game with a best 2 out of 3 format'
+# 2. What are some keywords and phrases ? 
+'random select from program'
+# 3. Is there input data that the question provides ?
+'we need to take in the users RPS selection'
+# 4. What is the desired output ?
+'shows which player won or loss'
+# 5. Pseudocode Steps & Additional Notes 
+' import random, random.range() random.choice'
+'needs to have logic to determine who won after three turns, as well ask the player if they want to play again WE NEED TO LOOP'
+'if / else conditions to determine which options win, ex i user selects rock and cpu selects paper == cpu wins'
+'Need a variable to track if user or cpu wins'
